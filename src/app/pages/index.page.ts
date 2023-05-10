a450a63c580aa7399074a7793fcb25a7453b166b
Chris Perko <chris@chrisperko.net>
2023-05-10 19:03:13 -0400

    target blank to links

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -12,10 +12,10 @@ import { Component } from '@angular/core';
       </div>
       <h1>Welcome!</h1>
       <p>My name is Chris Perko. I make things with Angular, and help others to do the same!</p>
-      <p>I work as a senior software engineering consultant for <a href="https://hero.dev">HeroDevs</a>, and I get
+      <p>I work as a senior software engineering consultant for <a href="https://hero.dev" target="_blank">HeroDevs</a>, and I get
         to help companies solve challenging problems every day with Angular.</p>
       <p>I also love sharing knowledge. You can find my blog here on this site, but I also help with the
-        <a href="https://angularcommunity.net/">Angular Community Meetup</a> as a co-organizer, as well
+        <a href="https://angularcommunity.net/" target="_blank">Angular Community Meetup</a> as a co-organizer, as well
         as give talks at conferences!</p>
     </section>
   `,
