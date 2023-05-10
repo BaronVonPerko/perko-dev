2f08ce059ce56550596d948fd8a8b3bd6601b24f
Chris Perko <chris@chrisperko.net>
2023-05-09 21:10:05 -0400

    Styling of front page text section

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -7,25 +7,41 @@ import { Component } from '@angular/core';
     <section class="hero">
     </section>
     <section class="about">
+      <div class="circles">
+        <span class="circle"></span><span class="circle"></span><span class="circle"></span>
+      </div>
       <h1>Welcome!</h1>
       <p>My name is Chris Perko.  I make things with Angular, and help others to do the same!</p>
     </section>
   `,
   styles: [`
-    section.hero {
-      background-image: url('/hero.jpeg');
-      min-height: 80vh;
-      background-size: cover;
-      background-attachment: fixed;
-    }
-    
-    section.about {
-      border: 4px solid var(--color-accent);
-      width: 60%;
-      margin: -80px 0 0 10%;
-      background-color: var(--color-primary);
-      padding: 20px;
-    }
+      section.hero {
+          background-image: url('/hero.jpeg');
+          min-height: 80vh;
+          background-size: cover;
+          background-attachment: fixed;
+      }
+
+      section.about {
+          border: 4px solid var(--color-accent);
+          width: 60%;
+          margin: -80px 0 0 10%;
+          background-color: var(--color-primary);
+          padding: 20px;
+      }
+
+      section.about .circles {
+          text-align: right;
+      }
+
+      section.about .circles .circle {
+          display: inline-block;
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+          border: 2px solid var(--color-accent);
+        margin: 0 5px;
+      }
   `],
 })
 export default class HomeComponent {
