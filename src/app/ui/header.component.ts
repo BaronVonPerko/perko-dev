10a6564f4380a4cf22939c52fecd5fad78c33ab2
Chris Perko <chris@chrisperko.net>
2023-05-09 20:41:28 -0400

    Styling of header component, introducing css vars

--- src/app/ui/header.component.ts
+++ src/app/ui/header.component.ts
@@ -6,19 +6,53 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <header>
-      <img src="/logo.png" alt="logo" />
-    </header>
+      <header>
+          <img src="/logo.png" alt="logo"/>
+          <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Talks</a></li>
+              <li><a href="#">About Me</a></li>
+              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Work History</a></li>
+              <li><a href="#">Contact</a></li>
+          </ul>
+      </header>
   `,
   styles: [
       `
           header {
-              background-color: #1e141f;
+              background-color: var(--color-primary);
+              padding: 0 40px;
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
           }
 
           img {
               max-width: 160px;
-          }`
+          }
+
+          ul {
+              list-style: none;
+              display: flex;
+          }
+
+          li {
+              margin: 0 20px;
+          }
+          
+          li a {
+              color: var(--color-accent);
+              font-size: 1.2rem;
+              font-weight: 700;
+              text-decoration: none;
+          }
+
+          li a:hover {
+              color: #6bcacb;
+          }
+      `
   ]
 })
 export class HeaderComponent {
