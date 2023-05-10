00631b4a2643ce3e762999eda8885b3fe7629106
Chris Perko <chris@chrisperko.net>
2023-05-09 21:02:33 -0400

    Text styling

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -5,16 +5,27 @@ import { Component } from '@angular/core';
   standalone: true,
   template: `
     <section class="hero">
-      
+    </section>
+    <section class="about">
+      <h1>Welcome!</h1>
+      <p>My name is Chris Perko.  I make things with Angular, and help others to do the same!</p>
     </section>
   `,
   styles: [`
-  section.hero {
-    background-image: url('/hero.jpeg');
-    min-height: 80vh;
-    background-size: cover;
-    background-attachment: fixed;
-  }
+    section.hero {
+      background-image: url('/hero.jpeg');
+      min-height: 80vh;
+      background-size: cover;
+      background-attachment: fixed;
+    }
+    
+    section.about {
+      border: 4px solid var(--color-accent);
+      width: 60%;
+      margin: -80px 0 0 10%;
+      background-color: var(--color-primary);
+      padding: 20px;
+    }
   `],
 })
 export default class HomeComponent {
