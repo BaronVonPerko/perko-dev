acd4d92e7746b3aa27bf13e00dfd876ca3fe68bc
Chris Perko <chris@chrisperko.net>
2023-05-15 20:58:52 -0400

    Updated about me

--- src/app/pages/(home).page.ts
+++ src/app/pages/(home).page.ts
@@ -9,14 +9,10 @@ import {ControlButtonsComponent} from '../ui/control-buttons.component';
     </section>
     <section class="about">
       <app-control-buttons />
-      <h1>Welcome!</h1>
-      <p>My name is Chris Perko. I make things with Angular, and help others to do the same!</p>
-      <p>I work as a senior software engineering consultant for <a href="https://hero.dev" target="_blank">HeroDevs</a>,
-        and I get
-        to help companies solve challenging problems every day with Angular.</p>
-      <p>I also love sharing knowledge. You can find my blog here on this site, but I also help with the
-        <a href="https://angularcommunity.net/" target="_blank">Angular Community Meetup</a> as a co-organizer, as well
-        as give talks at conferences!</p>
+      <h1>Hello, there!</h1>
+      <p>Hi there! I'm Chris Perko, a passionate and experienced software engineer who loves working with Angular. At <a href="https://hero.dev" target="_blank">HeroDevs</a>, I tackle complex and exciting challenges every day, using the latest technologies and best practices.</p>
+      <p>But that's not all! I also enjoy sharing my knowledge and enthusiasm with the Angular community. I co-organize the <a href="https://angularcommunity.net/" target="_blank">Angular Community Meetup</a>, where we host awesome speakers and events. And sometimes, I get to be one of those speakers myself! I've spoken at the meetup as well as <a href="https://ng-conf.org/" target="_blank">ng-conf</a> about Angular topics, and I always have a blast doing it.</p>
+      <p>If you want to know more about me or my work, feel free to contact me anytime. I'd love to hear from you!</p>
     </section>
   `,
   styles: [`
