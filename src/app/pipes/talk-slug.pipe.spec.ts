2ffaf8b99013565a6f25ca547e86da8b4a86beac
Chris Perko <chris@chrisperko.net>
2024-05-28 20:25:26 -0400

    Merge pull request #5
    * Upgrade Analog and related deps
    
    * Upgrade Angular to 17.3
    
    * Removed unused test files
    
    * Updated analog to 1.4.0
    
    * Update angular to v18

--- src/app/pipes/talk-slug.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TalkSlugPipe } from './talk-slug.pipe';
-
-describe('TalkSlugPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TalkSlugPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
