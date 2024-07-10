f024ecd60ab2022c5cac1000d9cd888426f6c574
baronvonperko <chris@chrisperko.net>
2024-07-10 15:20:29 -0400

    Setup light/dark theme switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,46 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {HeaderComponent} from './ui/header.component';
-import {FooterComponent} from './ui/footer.component';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./ui/footer.component";
+import { MatIcon } from "@angular/material/icon";
+import { MatIconButton } from "@angular/material/button";
+import { MatToolbar } from "@angular/material/toolbar";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet, FooterComponent, MatIcon, MatIconButton, MatToolbar],
     template: `
-        <app-header/>
+      <main [class.light-theme]="theme() === 'light'" [class.dark-theme]="theme() === 'dark'">
+        <mat-toolbar>
+          <span>perko.dev</span>
+
+          <span class="spacer"></span>
+
+          <a href="/blog">Blog</a>
+          <a href="/talks">Talks</a>
+          <a href="/portfolio">Portfolio</a>
+
+          <span class="spacer"></span>
+
+          <button mat-icon-button (click)="toggleTheme()">
+            @if (theme() === 'light') {
+              <mat-icon>dark_mode</mat-icon>
+            } @else {
+              <mat-icon>light_mode</mat-icon>
+            }
+          </button>
+        </mat-toolbar>
         <router-outlet></router-outlet>
-        <app-footer/>
+        <app-footer />
+      </main>
     `,
+    styles: `
+        .spacer {
+            flex: 1 1 auto;
+        }
+    `
 })
 export class AppComponent {
+    protected readonly theme = signal<'light' | 'dark'>('light');
+    protected toggleTheme = () => this.theme.set(this.theme() === 'light' ? 'dark' : 'light');
 }
